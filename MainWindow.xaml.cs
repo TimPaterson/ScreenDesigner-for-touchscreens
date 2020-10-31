6e9733c976c97ecafe64638639d5676f3ed15d40
Tim <tp@timpaterson.com>
2020-10-31 14:11:19 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using PatersonTech;
+using ScreenDesigner.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Xml;
+
+namespace ScreenDesigner
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		const string StrXmlFileFilter = "XML Files|*.xml|All Files|*.*";
+		const int ImageSpacing = 10;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// Set up file watcher to detect if loaded file changes
+			m_watcher = new FileSystemWatcher();
+			m_watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+			m_watcher.Changed += Watcher_OnChanged;
+			m_watcher.Created += Watcher_OnChanged;
+			m_watcher.Deleted += Watcher_OnChanged;
+			m_watcher.Renamed += Watcher_OnRenamed;
+			m_dispatcher = Dispatcher.CurrentDispatcher;
+		}
+
+		static T Clone<T>(T obj) where T : Visual
+		{
+			string xml = XamlWriter.Save(obj);
+			var xmlTextReader = new XmlTextReader(new StringReader(xml));
+			var deepCopyObject = (T)XamlReader.Load(xmlTextReader);
+			return deepCopyObject;
+		}
+
+
+		#region Private Fields
+
+		FileSystemWatcher m_watcher;
+		Dispatcher m_dispatcher;
+
+		#endregion
+
+
+		#region Private Methods
+
+		bool LoadXml(string strXmlFileName)
+		{
+			XmlDocument doc;
+			XmlImage parser;
+			List<XmlImage.NamedBitmap> Images;
+
+			doc = new XmlDocument();
+			try
+			{
+				doc.Load(strXmlFileName);
+			}
+			catch (XmlException exc)
+			{
+				// UNDONE: XML parsing error
+				Debug.WriteLine(exc.Message);
+				return true;	// file exists, remember it
+			}
+			catch (Exception exc)
+			{
+				Debug.WriteLine(exc.Message);
+				return false;
+			}
+
+			try
+			{
+				parser = new XmlImage();
+				Images = parser.ParseXml(doc);
+				//img1.Width = Images[0].Width;
+				//img1.Height = Images[0].Height;
+				img1.Source = Images[0].Bitmap;
+			}
+			catch (Exception exc)
+			{
+				// UNDONE: parsing error
+				Debug.WriteLine(exc.Message);
+			}
+			return true;
+		}
+
+		bool LoadNewFile(string strXmlFileName)
+		{
+			if (LoadXml(strXmlFileName))
+			{
+				Settings.Default.XmlFileName = strXmlFileName;
+				m_watcher.Path = System.IO.Path.GetDirectoryName(Settings.Default.XmlFileName);
+				m_watcher.Filter = System.IO.Path.GetFileName(Settings.Default.XmlFileName);
+				m_watcher.EnableRaisingEvents = true;
+				return true;
+			}
+
+			m_watcher.EnableRaisingEvents = false;
+			return false;
+		}
+
+		void LoadXmlDefault()
+		{
+			LoadXml(Settings.Default.XmlFileName);
+		}
+
+		#endregion
+
+
+		#region Event Handlers
+
+		void Watcher_OnChanged(object source, FileSystemEventArgs e)
+		{
+			m_dispatcher.InvokeAsync(LoadXmlDefault);
+		}
+
+		void Watcher_OnRenamed(object source, RenamedEventArgs e)
+		{
+			m_dispatcher.InvokeAsync(LoadXmlDefault);
+		}
+
+		private void Window_Initialized(object sender, EventArgs e)
+		{
+			// Read in saved per-user settings
+			if (!Settings.Default.SettingsUpgraded)
+			{
+				Settings.Default.Upgrade();
+				Settings.Default.SettingsUpgraded = true;
+				Settings.Default.Save();
+			}
+		}
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			// Initialized event is too early to set window placement.
+			// Loaded event has already drawn the window, so if we 
+			// wait that long we see it move.
+			base.OnSourceInitialized(e);
+			this.SetPlacement(Settings.Default.MainWindowPlacement);
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			LoadNewFile(Settings.Default.XmlFileName);
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			Settings.Default.MainWindowPlacement = this.GetPlacement();
+			Settings.Default.Save();
+		}
+
+		private void btnLoadXml_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg;
+
+			dlg = new Microsoft.Win32.OpenFileDialog();
+			dlg.Filter = StrXmlFileFilter;
+
+			try
+			{
+				dlg.InitialDirectory = System.IO.Path.GetDirectoryName(Settings.Default.XmlFileName);
+				dlg.FileName = System.IO.Path.GetFileName(Settings.Default.XmlFileName);
+			}
+			catch { }
+
+			if (dlg.ShowDialog(this) == true)
+			{
+				if (!LoadNewFile(dlg.FileName))
+				{
+					// UNDONE: file error
+				}
+			}
+		}
+
+		private void btnSave_Click(object sender, RoutedEventArgs e)
+		{
+			BmpBitmapEncoder encode = new BmpBitmapEncoder();
+			encode.Frames.Add(BitmapFrame.Create((RenderTargetBitmap)img1.Source));
+			using (Stream stream = File.OpenWrite("Output.bmp"))
+				encode.Save(stream);
+		}
+
+		private void btnBrowse_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		#endregion
+	}
+}
---
+++ XmlImage.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ScreenDesigner
+{
+	class XmlImage
+	{
+		#region Types
+
+		class XmlGraphic
+		{
+			public virtual int Height 
+			{ 
+				get { return (int)Graphic.Height; }
+				set { Graphic.Height = value; }
+			}
+
+			public virtual int Width
+			{ 
+				get { return (int)Graphic.Width; }
+				set { Graphic.Width = value; }
+			}
+
+			public FrameworkElement Graphic { get; set; }
+			public virtual Element Owner { get; set; }
+
+			public void SetAttribute(string Attr, string Value)
+			{
+				PropertyInfo prop;
+				object obj;
+
+				prop = GetType().GetProperty(Attr);
+				if (prop != null)
+					obj = this;
+				else
+				{
+					prop = Graphic?.GetType().GetProperty(Attr);
+					obj = Graphic;
+				}
+
+				if (prop != null)
+				{
+					if (prop.PropertyType == typeof(string))
+						prop.SetValue(obj, Value);
+					else
+						prop.SetValue(obj, Convert.ChangeType(int.Parse(Value), prop.PropertyType));
+				}
+			}
+
+			public void Draw(ContainerVisual DrawList, int x, int y)
+			{
+				if (Graphic != null)
+				{
+					Graphic.Arrange(new Rect(x, y, Width, Height));
+					DrawList.Children.Add(Graphic);
+				}
+			}
+
+			protected static Color ColorFromString(string color)
+			{
+				int val;
+
+				val = (int)new Int32Converter().ConvertFromString(color);
+				return Color.FromRgb((byte)(val >> 16), (byte)((val >> 8) & 0xff), (byte)(val & 0xff));
+			}
+		}
+
+		class XmlRectangle : XmlGraphic
+		{
+			public XmlRectangle()
+			{
+				Rectangle rect;
+
+				rect = new Rectangle();
+				rect.StrokeThickness = 0;
+				Graphic = rect;
+			}
+
+			public string Stroke
+			{
+				set { ((Rectangle)Graphic).Stroke = new SolidColorBrush(ColorFromString(value)); }
+			}
+
+			public string Fill
+			{
+				set { ((Rectangle)Graphic).Fill = new SolidColorBrush(ColorFromString(value)); }
+			}
+		}
+
+		class XmlTextBlock : XmlGraphic
+		{
+			public XmlTextBlock()
+			{
+				Graphic = new TextBlock();
+				Graphic.HorizontalAlignment = HorizontalAlignment.Center;
+				Graphic.VerticalAlignment = VerticalAlignment.Center;
+			}
+			public override int Height { get; set; }
+			public override int Width { get; set; }
+
+			public override Element Owner 
+			{
+				get { return base.Owner; }
+				set
+				{
+					base.Owner = value;
+					Width = Owner.Parent.Graphic.Width;
+					Height = Owner.Parent.Graphic.Height;
+				}
+			}
+		}
+
+		class XmlHotSpot : XmlGraphic
+		{
+			public override int Height { get; set; }
+			public override int Width { get; set; }
+
+		}
+
+		class Element
+		{
+			internal class ChildCollection : KeyedCollection<string, Element>
+			{
+				protected override string GetKeyForItem(Element item)
+				{
+					return item.Name;
+				}
+			}
+			
+			public Element(string type, Element parent)
+			{
+				Type typGraphic;
+
+				Parent = parent;
+				if (ElementTypes.TryGetValue(type, out typGraphic))
+				{
+					Graphic = (XmlGraphic)Activator.CreateInstance(typGraphic);
+					Graphic.Owner = this;
+				}
+				Children = new ChildCollection();
+			}
+
+			#region Public Properties
+
+			public string Name { get; set; }
+			public int Top { get; set; }
+			public int Left { get; set; }
+			public int Width { get { return (int)Graphic.Width; } }
+			public int Height { get { return (int)Graphic.Height; } }
+			public ChildCollection Children { get; protected set; }
+			public XmlGraphic Graphic { get; set; }
+			public Element Parent { get; set; }
+
+			#endregion
+
+			static readonly Dictionary<string, Type> ElementTypes = new Dictionary<string, Type>
+			{
+				{ "Rectangle",	typeof(XmlRectangle) },
+				{ "TextBlock",	typeof(XmlTextBlock) },
+				{ "HotSpot",	typeof(XmlHotSpot) },
+				{ "Image",		typeof(XmlRectangle) },
+			};
+
+			public void SetAttribute(string Attr, string Value)
+			{
+				Graphic?.SetAttribute(Attr, Value);
+
+				// Assign our own properties
+				switch (Attr)
+				{
+					case "Name":
+						Name = Value;
+						break;
+
+					case "Top":
+						Top = int.Parse(Value);
+						break;
+
+					case "Left":
+						Left = int.Parse(Value);
+						break;
+				}
+			}
+
+			public void Draw(ContainerVisual DrawList, int x, int y)
+			{
+				x += Left;
+				y += Top;
+				Graphic.Draw(DrawList, x, y);
+				foreach (Element el in Children)
+					el.Draw(DrawList, x, y);
+			}
+		}
+
+		internal class NamedBitmap
+		{
+			public NamedBitmap(string name, int width, int height)
+			{
+				Name = name;
+				Height = height;
+				Width = width;
+				Bitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+			}
+			public string Name { get; set; }
+			public int Height { get; set; }
+			public int Width { get; set; }
+			public RenderTargetBitmap Bitmap { get; set; }
+		}
+
+		#endregion
+
+
+		#region Constructor
+
+		public XmlImage()
+		{
+			Components = new Dictionary<string, Element>();
+			Images = new List<NamedBitmap>();
+		}
+
+		#endregion
+
+
+		#region Fields
+
+		Dictionary<string, Element> Components;
+		List<NamedBitmap> Images;
+
+		#endregion
+
+
+		#region Public Methods
+
+		internal List<NamedBitmap> ParseXml(XmlDocument xml)
+		{
+			XmlNode elProject;
+
+			elProject = xml.DocumentElement;
+			foreach (XmlNode el in elProject.ChildNodes)
+			{
+				switch (el.Name)
+				{
+					case "Component":
+						DefineComponent(el);
+						break;
+
+					case "Image":
+						DefineImage(el);
+						break;
+				}
+			}
+			return Images;
+		}
+
+		#endregion
+
+
+		#region Private Methods
+
+		void DefineComponent(XmlNode node)
+		{
+			Element el;
+
+			el = BuildElement(node);
+			Components.Add(el.Name, el);
+		}
+
+		void DefineImage(XmlNode node)
+		{
+			Element el;
+			ContainerVisual DrawList;
+			NamedBitmap bmp;
+
+			el = BuildElement(node);
+			DrawList = new ContainerVisual();
+			el.Draw(DrawList, 0, 0);
+			bmp = new NamedBitmap(el.Name, el.Width, el.Height);
+			bmp.Bitmap.Render(DrawList);
+			Images.Add(bmp);
+		}
+
+		Element BuildElement(XmlNode node, Element parent = null)
+		{
+			Element el;
+
+			el = new Element(node.Name, parent);
+			foreach (XmlAttribute attr in node.Attributes)
+				el.SetAttribute(attr.Name, attr.Value);
+
+			foreach (XmlNode nodeChild in node.ChildNodes)
+				el.Children.Add(BuildElement(nodeChild, el));
+
+			return el;
+		}
+
+		#endregion
+	}
+}
